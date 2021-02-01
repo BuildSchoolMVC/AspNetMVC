6cd306553ec439fd92d48349c977c3d9ec847507
Mike Lee <leemike0429@gmail.com>
2021-02-01 18:18:15 +0800

    補上完整的JS

--- AspNetMVC/Scripts/ProductPage/ProductPage.js
+++ AspNetMVC/Scripts/ProductPage/ProductPage.js
@@ -7,14 +7,19 @@ var itemgroupgrid = document.getElementById("item-group-grid");
 var itemgrouplist = document.getElementById("item-group-list");
 var gridbtn = document.getElementById("grid-btn");
 var listbtn = document.getElementById("list-btn");
-
+var todefinebtn = document.getElementById("todefinebutton");
+var topackagebtn = document.getElementById("topackagebutton")
+var packageproduct = document.getElementById("packageproduct")
+var definedproduct = document.getElementById("definedproduct")
+var viewedarea = document.getElementById("viewed-area")
+var viewedareabtn = document.getElementById("viewed-area-btn")
 ////載入區
 window.onload = function () {
-    sideMenuContorl()
-    viewModalSwitch()
+    setMenuContorl()
+    viewModeSwitch()
+    shopModeSwitch()
+    setViewedContorl()
 }
-
-
 
 ////操作區
 
@@ -33,21 +38,29 @@ function sideMenuShow() {
 
 
 //側邊欄監聽
-function sideMenuContorl() {
+function setMenuContorl() {
     if (window.innerWidth >= 768) {
         sideMenuShow()
-    } else {
-
-        sidemenubtn.addEventListener("click", function () {
-
-            if ($(sidemenu).hasClass("close")) {
-                sideMenuShow()
-            } else {
-                sideMenuHidden()
-            }
-        })
     }
 }
+//監聽視窗尺寸
+window.addEventListener("resize", function () {
+    setViewedContorl()
+    viewedAreaHidden()
+    if (window.innerWidth >= 768) {
+        sideMenuHidden()
+        sideMenuShow()
+
+    }
+})
+//監聽側邊欄按鈕
+sidemenubtn.addEventListener("click", function () {
+    if ($(sidemenu).hasClass("close")) {
+        sideMenuShow()
+    } else {
+        sideMenuHidden()
+    }
+})
 
 
 
@@ -67,7 +80,7 @@ function viewList() {
 }
 
 //Grid/List監聽
-function viewModalSwitch() {
+function viewModeSwitch() {
     gridbtn.addEventListener("click", function () {
 
         if (!$(itemgrouplist).hasClass("d-none")) {
@@ -89,4 +102,67 @@ function viewModalSwitch() {
 
 ////轉換區+瀏覽區
 
+function setViewedContorl() {
+    if (window.innerWidth <= 768) {
+        viewedareabtn.addEventListener("click", function () {
+            if ($(viewedarea).hasClass("open")) {
+                viewedAreaHidden()
+            } else {
+                viewedAreaShow()
+            }
+        })
+    }
+}
+
+//瀏覽區監聽
+// viewedareabtn.addEventListener("click", function () {
+//     if ($(viewedarea).hasClass("open")) {
+//         viewedAreaHidden()
+//     } else {
+//         viewedAreaShow()
+//     }
+// })
+
+//瀏覽區打開
+function viewedAreaShow() {
+    viewedarea.classList.add("open");
+}
+//瀏覽區關閉
+function viewedAreaHidden() {
+    viewedarea.classList.remove("open");
+}
+
+
+
+
 //切換套裝/客製化
+function shopInDefine() {
+    packageproduct.classList.add("d-none")
+    todefinebtn.classList.add("d-none")
+    definedproduct.classList.remove("d-none")
+    topackagebtn.classList.remove("d-none")
+}
+//用List顯示
+function shopInPackage() {
+    packageproduct.classList.remove("d-none")
+    todefinebtn.classList.remove("d-none")
+    definedproduct.classList.add("d-none")
+    topackagebtn.classList.add("d-none")
+}
+//購物模式切換
+function shopModeSwitch() {
+    todefinebtn.addEventListener("click", function () {
+
+        if (!$(packageproduct).hasClass("d-none")) {
+            shopInDefine()
+        }
+
+    })
+
+    topackagebtn.addEventListener("click", function () {
+        if (!$(definedproduct).hasClass("d-none")) {
+            shopInPackage()
+        }
+    })
+
+}
