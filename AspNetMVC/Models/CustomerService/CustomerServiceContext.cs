6d3d8b85f82fd4b5cfeba014b3dbf15ed0e7e384
Jacko <jacko1114@hotmail.com>
2021-02-05 18:49:00 +0800

    加入客服表單系統 + 修正部分問題

--- AspNetMVC/Models/CustomerService/CustomerServiceContext.cs
+++ AspNetMVC/Models/CustomerService/CustomerServiceContext.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
-using System.IO;
 
 namespace AspNetMVC.Models.CustomerService
 {
     public partial class CustomerServiceContext :DbContext
     {
-        public CustomerServiceContext() : base("name=CustomerServiceConnection")
+        public CustomerServiceContext() : base("name=CustomerServiceConn")
         {
-            //AppDomain.CurrentDomain.SetData("DataDirectory", Directory.GetCurrentDirectory());
         }
 
         public virtual DbSet<CustomerService> CustomerServices { get; set; }
