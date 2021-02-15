2e7f998c49e009f2836587f174d0634350645df4
Shrank Kao <a23057475@gmail.com>
2021-02-15 14:16:37 +0800

    新增訂單CSS

---
+++ AspNetMVC/Models/Member/MemberMd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVC.Models.Member
+{
+    public class MemberMd
+    {
+        public string name { get; set; }
+        public int phone { get; set; }
+        public string mail {get;set;}
+        public string address { get; set; }
+        public Array reservationOrder { get; set; }
+        public Array planOrder { get; set; }
+        public Array finishOrder { get; set; }
+        public string password { get; set; }
+        public int creditNumber { get; set; }
+        public DateTime 
+    }
+}
