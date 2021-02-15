6da6273f4e78404f2b0b69d65e2b0fae9008e9fb
Xian-En Lin <blueeye851114@gmail.com>
2021-02-15 17:01:26 +0800

    修正js檔

--- AspNetMVC/Assets/js/Checkout/all.js
+++ AspNetMVC/Assets/js/Checkout/all.js
@@ -9,157 +9,32 @@ const disableBtn = (btn) => {
   btn.attr('disabled', '');
   btn.addClass('disable');
 };
-const $countyList = $('#county-list');
-const $districtList = $('#district-list');
-const county = [{
-    "name": "臺北市",
-    "districts": [{
-        "name": "中正區"
-      },
-      {
-        "name": "大同區"
-      },
-      {
-        "name": "中山區"
-      },
-      {
-        "name": "松山區"
-      },
-      {
-        "name": "大安區"
-      },
-      {
-        "name": "萬華區"
-      },
-      {
-        "name": "信義區"
-      },
-      {
-        "name": "士林區"
-      },
-      {
-        "name": "北投區"
-      },
-      {
-        "name": "內湖區"
-      },
-      {
-        "name": "南港區"
-      },
-      {
-        "name": "文山區"
-      }
-    ]
-  },
-  {
-    "name": "新北市",
-    "districts": [{
-        "name": "萬里區"
-      },
-      {
-        "name": "金山區"
-      },
-      {
-        "name": "板橋區"
-      },
-      {
-        "name": "汐止區"
-      },
-      {
-        "name": "深坑區"
-      },
-      {
-        "name": "石碇區"
-      },
-      {
-        "name": "瑞芳區"
-      },
-      {
-        "name": "平溪區"
-      },
-      {
-        "name": "雙溪區"
-      },
-      {
-        "name": "貢寮區"
-      },
-      {
-        "name": "新店區"
-      },
-      {
-        "name": "坪林區"
-      },
-      {
-        "name": "烏來區"
-      },
-      {
-        "name": "永和區"
-      },
-      {
-        "name": "中和區"
-      },
-      {
-        "name": "土城區"
-      },
-      {
-        "name": "三峽區"
-      },
-      {
-        "name": "樹林區"
-      },
-      {
-        "name": "鶯歌區"
-      },
-      {
-        "name": "三重區"
-      },
-      {
-        "name": "新莊區"
-      },
-      {
-        "name": "泰山區"
-      },
-      {
-        "name": "林口區"
-      },
-      {
-        "name": "蘆洲區"
-      },
-      {
-        "name": "五股區"
-      },
-      {
-        "name": "八里區"
-      },
-      {
-        "name": "淡水區"
-      },
-      {
-        "name": "三芝區"
-      },
-      {
-        "name": "石門區"
-      }
-    ]
+$.ajax({
+  type: 'POST',
+  url: '/Checkout/getDistricts',
+  dataType: 'json',
+  success: (data) => {
+    const $countyList = $('#county-list');
+    const $districtList = $('#district-list');
+    data.forEach((item) => {
+      let option = document.createElement('option');
+      option.text = item.Name;
+      $countyList[0].add(option);
+    });
+    $countyList.on('change', function() {
+      let districtList = $districtList[0];
+      let prompt = districtList.firstElementChild;
+      $districtList.empty();
+      $districtList.append(prompt);
+      data[this.selectedIndex - 1].Districts.forEach((item) => {
+        let option = document.createElement('option');
+        option.text = item;
+        districtList.add(option);
+      });
+    });
   }
-];
-county.forEach((item) => {
-  let option = document.createElement('option');
-  option.text = item.name;
-  $countyList[0].add(option);
 });
-$countyList.on('change', function() {
-  let districtList = $districtList[0];
-  let prompt = districtList.firstElementChild;
-  $districtList.empty();
-  $districtList.append(prompt);
-  county[this.selectedIndex - 1].districts.forEach((item) => {
-    let option = document.createElement('option');
-    option.text = item.name;
-    districtList.add(option);
-  });
-});
-// 資訊區
+// model區
 const dateService = {
   obj: new Date(),
   date: null,
@@ -416,35 +291,36 @@ const isComplete = function() {
         toastr.error('請選擇日期');
         return false;
       }
-      case 1:
-        steps[1]['inputs'].forEach((item) => {
-          if (!item.check(item.ele)) {
-            ok = false;
-            $(item.ele).next('.warn').text(item.warn);
-          } else {
-            $(item.ele).next('.warn').text('');
-          }
-        });
-        return ok;
-      case 2:
-        const payMethod = $('#pay input[name=pay-method]:checked').attr('data-inputs');
-        steps[2][payMethod].forEach((item) => {
-          if (!item.check(item.ele)) {
-            ok = false;
-            $(item.ele).next('.warn').text(item.warn);
-          } else {
-            $(item.ele).next('.warn').text('');
-          }
-        });
-        steps[2]['agrees'].forEach((item) => {
-          if (!item.check(item.ele)) {
-            ok = false;
-            $(item.ele).siblings('.warn').text(item.warn);
-          } else {
-            $(item.ele).siblings('.warn').text('');
-          }
-        });
-        return ok;
+      break;
+    case 1:
+      steps[1]['inputs'].forEach((item) => {
+        if (!item.check(item.ele)) {
+          ok = false;
+          $(item.ele).next('.warn').text(item.warn);
+        } else {
+          $(item.ele).next('.warn').text('');
+        }
+      });
+      return ok;
+    case 2:
+      const payMethod = $('#pay input[name=pay-method]:checked').attr('data-inputs');
+      steps[2][payMethod].forEach((item) => {
+        if (!item.check(item.ele)) {
+          ok = false;
+          $(item.ele).next('.warn').text(item.warn);
+        } else {
+          $(item.ele).next('.warn').text('');
+        }
+      });
+      steps[2]['agrees'].forEach((item) => {
+        if (!item.check(item.ele)) {
+          ok = false;
+          $(item.ele).siblings('.warn').text(item.warn);
+        } else {
+          $(item.ele).siblings('.warn').text('');
+        }
+      });
+      return ok;
   }
   return false;
 };
