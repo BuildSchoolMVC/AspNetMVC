4c9e665b4dc8ac89f5db72f36159ae9ccc699f21
Xian-En Lin <blueeye851114@gmail.com>
2021-02-17 20:29:47 +0800

    OrderDetail

--- AspNetMVC/Models/Entity/Order.cs
+++ AspNetMVC/Models/Entity/Order.cs
@@ -7,69 +7,74 @@ using System.Web;
 namespace AspNetMVC.Models.Entity {
 	public class Order : BaseEnity {
 		[Key]
-		public int OrderId { get; set; }
+		public Guid OrderId { get; set; }
 
 		[Required]
 		public Guid AccountId { get; set; }
-
-		public int FavoriteId { get; set; }
-
-		[Required]
-		public DateTime DateCreated { get; set; }
 
 		[Required]
 		public DateTime DateService { get; set; }
 
 		[Required]
-		public DateTime TimeService { get; set; }
-
-		[Required]
 		public string Address { get; set; }
 
 		[Required]
-		public OrderState OrderState { get; set; }
+		public byte OrderState { get; set; }
+		//待付款 0
+		//已付款 1
+		//已取消 2
 
 		public byte Rate { get; set; }
 
 		public string Comment { get; set; }
 
-		public int CouponID { get; set; }
+		public Guid CouponID { get; set; }
 
 		[Required]
-		public PaymentMethod PaymentMethod { get; set; }
+		public byte PaymentMethod { get; set; }
+		//信用卡 0
+		//ATM 1
 
 		[Required]
-		public InvoiceType InvoiceType { get; set; }
+		public byte InvoiceType { get; set; }
+		//個人電子發票 0
+		//捐贈 1
+		//載具發票 2
 
-		public InvoiceDonateTo InvoiceDonateTo { get; set; }
+		public byte InvoiceDonateTo { get; set; }
+		//中華民國唐氏症基金會 0
+		//陽光社會福利基金會 1
+		//台灣兒童暨家庭扶助基金會 2
 
 	}
-	public enum OrderState {
-		//待付款
-		PendingPayment,
-		//已付款
-		AlreadyPaid,
-		//已取消
-		Cancelled,
-	}
-	public enum PaymentMethod {
-		CreditCard,
-		ATM,
-	}
-	public enum InvoiceType {
-		//個人電子發票
-		Personal,
-		//載具發票
-		Carrier,
-		//捐贈
-		Donate,
-	}
-	public enum InvoiceDonateTo {
-		//中華民國唐氏症基金會
-		DownSyndrome,
-		//陽光社會福利基金會
-		SunshineSocial,
-		//台灣兒童暨家庭扶助基金會
-		ChildrenFamilies,
-	}
+	//public enum OrderState {
+	//  //
+	//	PendingPayment,
+	//	//
+	//	AlreadyPaid,
+	//	//
+	//	Cancelled,
+	//}
+	//public enum PaymentMethod {
+	//  //
+	//	CreditCard,
+	//	//
+	//	ATM,
+	//}
+	//public enum InvoiceType {
+	//	//
+	//	Personal,
+	//	//
+	//	Carrier,
+	//	//
+	//	Donate,
+	//}
+	//public enum InvoiceDonateTo {
+	//	
+	//	DownSyndrome,
+	//	
+	//	SunshineSocial,
+	//	
+	//	ChildrenFamilies,
+	//}
 }
--- AspNetMVC/Models/Entity/OrderDetail.cs
+++ AspNetMVC/Models/Entity/OrderDetail.cs
@@ -6,20 +6,15 @@ using System.Web;
 
 namespace AspNetMVC.Models.Entity {
 	public class OrderDetail : BaseEnity {
+		[Key]
 		[Required]
-		public int OrderId { get; set; }
+		public Guid OrderDetailId { get; set; }
 		[Required]
-		public int RoomTypeId { get; set; }
-		[Required]
-		public int ServiceItems { get; set; }
-		[Required]
-		public int SquareFeet { get; set; }
-		[Required]
-		public double Hours { get; set; }
-		[Required]
-		public int PackageID { get; set; }
-		[Required]
-		public int ProductID { get; set; }
+		public Guid OrderId { get; set; }
+
+		public Guid UserDefinedId { get; set; }
+
+		public int PackageProductId { get; set; }
 		[Required]
 		public decimal ProductPrice { get; set; }
 		[Required]
