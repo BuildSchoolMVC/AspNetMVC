7ef70067a2a7f86cb28c55dfb0cfa98f2e9a8c5f
Jacko <jacko1114@hotmail.com>
2021-02-17 19:34:22 +0800

    改錯字

--- AspNetMVC/Models/Entity/OrderDetail.cs
+++ AspNetMVC/Models/Entity/OrderDetail.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace AspNetMVC.Models.Entity
 {
-    public class OrderDetail : BaseEnity
+    public class OrderDetail : BaseEntity
     {
         [Key]
         [Required]
