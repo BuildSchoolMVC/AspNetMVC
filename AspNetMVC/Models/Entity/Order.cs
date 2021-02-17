8f681d3e6ba3e8be301559aed0f6cac35a55bfb3
Xian-En Lin <blueeye851114@gmail.com>
2021-02-17 10:50:29 +0800

    初步建立Entity

---
+++ AspNetMVC/Models/Entity/Order.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVC.Models.Entity {
+	public class Order : BaseEnity {
+		[Key]
+		public int OrderId { get; set; }
+
+		[Required]
+		public Guid AccountId { get; set; }
+
+		public int FavoriteId { get; set; }
+
+		[Required]
+		public DateTime DateCreated { get; set; }
+
+		[Required]
+		public DateTime DateService { get; set; }
+
+		[Required]
+		public DateTime TimeService { get; set; }
+
+		[Required]
+		public string Address { get; set; }
+
+		[Required]
+		public OrderState OrderState { get; set; }
+
+		public byte Rate { get; set; }
+
+		public string Comment { get; set; }
+
+		public int CouponID { get; set; }
+
+		[Required]
+		public PaymentMethod PaymentMethod { get; set; }
+
+		[Required]
+		public InvoiceType InvoiceType { get; set; }
+
+		public InvoiceDonateTo InvoiceDonateTo { get; set; }
+
+	}
+	public enum OrderState {
+		//待付款
+		PendingPayment,
+		//已付款
+		AlreadyPaid,
+		//已取消
+		Cancelled,
+	}
+	public enum PaymentMethod {
+		CreditCard,
+		ATM,
+	}
+	public enum InvoiceType {
+		//個人電子發票
+		Personal,
+		//載具發票
+		Carrier,
+		//捐贈
+		Donate,
+	}
+	public enum InvoiceDonateTo {
+		//中華民國唐氏症基金會
+		DownSyndrome,
+		//陽光社會福利基金會
+		SunshineSocial,
+		//台灣兒童暨家庭扶助基金會
+		ChildrenFamilies,
+	}
+}
---
+++ AspNetMVC/Models/Entity/OrderDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVC.Models.Entity {
+	public class OrderDetail : BaseEnity {
+		[Required]
+		public int OrderId { get; set; }
+		[Required]
+		public int RoomTypeId { get; set; }
+		[Required]
+		public int ServiceItems { get; set; }
+		[Required]
+		public int SquareFeet { get; set; }
+		[Required]
+		public double Hours { get; set; }
+		[Required]
+		public int PackageID { get; set; }
+		[Required]
+		public int ProductID { get; set; }
+		[Required]
+		public decimal ProductPrice { get; set; }
+		[Required]
+		public string ProductName { get; set; }
+		[Required]
+		public bool IsPakage { get; set; }
+	}
+}
