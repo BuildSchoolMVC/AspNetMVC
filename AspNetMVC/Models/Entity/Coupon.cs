01172c80889e24c225c368003ce9f96ba569ff30
Xian-En Lin <blueeye851114@gmail.com>
2021-02-21 12:07:38 +0800

    Coupon表migration

--- AspNetMVC/Models/Entity/Coupon.cs
+++ AspNetMVC/Models/Entity/Coupon.cs
@@ -7,7 +7,7 @@ using System.Web;
 namespace AspNetMVC.Models.Entity {
 	public class Coupon : BaseEntity {
 		[Key]
-		public Guid CouponID { get; set; }
+		public int CouponId { get; set; }
 		public string CouponName { get; set; }
 		public decimal DiscountAmount { get; set; }
 		public DateTime DateStart { get; set; }
--- AspNetMVC/Models/Entity/CouponDetail.cs
+++ AspNetMVC/Models/Entity/CouponDetail.cs
@@ -7,12 +7,17 @@ using System.Web;
 namespace AspNetMVC.Models.Entity {
 	public class CouponDetail : BaseEntity {
 		[Key]
-		public Guid CouponDetailID { get; set; }
-		public int CouponID { get; set; }
+		public Guid CouponDetailId { get; set; }
+		public int CouponId { get; set; }
 		public string AccountName { get; set; }
 		public int State { get; set; }
 		//0 未使用
 		//1 已使用
 		//2 已過期
+	}
+	public enum UseState {
+		Unused,
+		Used,
+		Expired,
 	}
 }
