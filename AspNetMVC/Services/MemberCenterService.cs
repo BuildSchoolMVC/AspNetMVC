fa64a249078dcedc85a29fa2ef0bbf03d6343045
Shrank Kao <a23057475@gmail.com>
2021-02-25 11:03:57 +0800

    修改密碼完成

--- AspNetMVC/Services/MemberCenterService.cs
+++ AspNetMVC/Services/MemberCenterService.cs
@@ -14,6 +14,7 @@ namespace AspNetMVC.Services
     {
         private readonly UCleanerDBContext _context;
         private readonly BaseRepository _repository;
+        public List<Account> _accounts { get; set; }
 
         public MemberCenterService() {
             _context = new UCleanerDBContext();
@@ -30,6 +31,11 @@ namespace AspNetMVC.Services
             };
             return result;
         }
+        public Account GetAll(Guid accountId)
+        {
+            var source = _repository.GetAll<Account>().FirstOrDefault(x => x.AccountId == accountId);
+            return source;
+        }
         public MemberMd SaveModel(Guid accountId,MemberCenterViewModels memberVm) 
         {
             var source = _repository.GetAll<MemberMd>().FirstOrDefault(x => x.AccountId == accountId);
@@ -40,6 +46,29 @@ namespace AspNetMVC.Services
             _repository.Update<MemberMd>(source);
             _context.SaveChanges();
             return source;
+        }
+        public OperationResult EditPassword(Guid accountId,MemberCenterPassword password)
+        {
+            var result = new OperationResult();
+            var source = _repository.GetAll<Account>().FirstOrDefault(x => x.AccountId == accountId);
+            var orginPassword = password.Password;
+            var Md5OrginPassword = Helpers.ToMD5(orginPassword);
+            var newPassword = password.NewPassword;
+            var Md5NewPassword = Helpers.ToMD5(newPassword);
+            var confirmPassword = password.ConfirmPassword;
+            
+            if (Md5OrginPassword == source.Password)
+            {
+                source.Password = Md5NewPassword;
+                _repository.Update<Account>(source);
+                _context.SaveChanges();
+                result.IsSuccessful = true;
+            }
+            else
+            {
+                result.IsSuccessful = false;
+            }
+            return result;
         }
     }
 }
