91e92d71b31bbd3947cdc71701d300c8c39fc34f
asn338053 <asn338053@gmail.com>
2021-02-26 16:30:36 +0800

    coupon status judgment

--- AspNetMVC/Repository/FakeData/CouponRepository.cs
+++ AspNetMVC/Repository/FakeData/CouponRepository.cs
@@ -13,7 +13,8 @@ namespace AspNetMVC.Repository
             return new List<CouponViewModel>
             {
                new CouponViewModel{CouponName="Anderson",DiscountAmount=100m, DateStart=new DateTime(2021,01,01),DateEnd=new DateTime(2021,12,31)},
-               //new CouponViewModel{CouponName="Peter",DiscountAmount=-200m, DateStart=new DateTime(2021,01,01),DateEnd=new DateTime(2022,12,31)},  
+               new CouponViewModel{CouponName="Anderson",DiscountAmount=100m, DateStart=new DateTime(2021,01,01),DateEnd=new DateTime(2022,12,31)},
+               new CouponViewModel{CouponName="Anderson",DiscountAmount=100m, DateStart=new DateTime(2021,01,01),DateEnd=new DateTime(2023,12,31)},
             };
         }
 
--- AspNetMVC/ViewModels/CouponViewModel.cs
+++ AspNetMVC/ViewModels/CouponViewModel.cs
@@ -17,6 +17,13 @@ namespace AspNetMVC.ViewModels
         public DateTime DateStart { get; set; }
 
         public DateTime DateEnd { get; set; }
+        /// <summary>
+        /// 優惠卷狀態
+        /// </summary>
+        //0 未使用
+        //1 已使用
+        //2 已過期
+        public int Status { get; set; }
 
     }
 }
